f738f14674c43e1e30c10530fcd3b6ea5b70fd44
Patrick Meyers <patrick.meyers@ligo.org>
2016-04-17 13:47:41 -0700

    update?

--- seispy/version.py
+++ seispy/version.py
@@ -5,10 +5,10 @@
 """
 
 __author__ = 'Duncan Macleod <duncan.macleod@ligo.org>'
-__version__ = '5624a667d28fdbd77e35b260948ba84ab0f1954f'
-__date__ = '2016-04-17 01:01:36 +0000'
+__version__ = '17e03eb87350f386bf3215b957cd8af968f9e48a'
+__date__ = '2016-04-17 16:07:22 +0000'
 
-git_id = '5624a667d28fdbd77e35b260948ba84ab0f1954f'
+git_id = '17e03eb87350f386bf3215b957cd8af968f9e48a'
 git_branch = 'master'
 git_tag = None
 git_author = 'Patrick Meyers <pmeyers279@gmail.com>'
